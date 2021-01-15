b4fcd5aae8d3e66c19c004fa82c84654d383e2ae
Florianbracke <florianbracke@gmail.com>
2021-01-15 11:04:24 +0100

    first commit

---
+++ guessing-game/view.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Casino royale - guessing game</title>
+</head>
+<body>
+
+<form method="post" action="">
+			Guess: <input type="number" min="1" max="10" name="guess">
+			<input type="submit">
+</form>
+
+</body>
+</html>
